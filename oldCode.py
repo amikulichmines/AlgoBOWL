f72ba79a1837ac4dcfb5e720ed3f8e914d6bcf2f
amikulichmines <amikulich@mymail.mines.edu>
2021-02-27 01:57:27 -0700

    Fixed it

---
+++ oldCode.py
@@ -0,0 +1,27 @@
+
+
+i=0 #outputIdx
+# inputIdx=0
+# while inputIdx!=len(inputs):
+#     n = inputs[inputIdx]
+#     if out3[i]==n:
+#         inputIdx+=1
+#
+#     elif 2*out3[i]>n:
+#         dif=2*out3[i]-n
+#         x=binary_search_boolean([1]+out3,dif)
+#         if x:
+#             i+=1
+#             out3.append(out3[x]+out3[i])
+#             out2.append(out3[i])
+#             out1.append(out3[x])
+#         else:
+#             while not addUp([1]+out3,dif):
+#
+#
+#     elif 2*out3[i]<n:
+#         out3.append(2*out3[i])
+#         out1.append(out3[i])
+#         out2.append(out3[i])
+#         i+=1
+#         inputIdx+=1
--- solve.py
+++ solve.py
@@ -1,10 +1,13 @@
-def addUp(s, x):
+import random
+
+def addUp(s, diff):
     for element in s:
-        x = binary_search_boolean(s, x-element)
+        x = binary_search_boolean(s, diff-element)
         if x:
             return (s[x],element)
     return False
     # Complexity works out to O(nlog(n)) + O(nlog(n)), so just O(nlog(n))
+
 
 def binary_search_boolean(arr, x):
     # Binary search
@@ -22,40 +25,62 @@ def binary_search_boolean(arr, x):
             return mid
     return False
 
-with open('input.txt','r') as f:
-    i=0
-    for line in f:
-        if(i!=0):
-            inputs=[int(elem) for elem in list(line.split(','))]
-        i+=1
-
-
-out1 = [1]
-out2 = [1]
-out3 = [2]
-i=0 #outputIdx
-inputIdx=0
-while inputIdx!=len(inputs):
-    n = inputs[inputIdx]
-    if out3[i]==n:
-        inputIdx+=1
-
-    elif 2*out3[i]>n:
-        dif=2*out3[i]-n
-        x=binary_search_boolean([1]+out3,dif)
-        if x:
-            i+=1
-            out3.append(out3[x]+out3[i])
-            out2.append(out3[i])
-            out1.append(out3[x])
+def fixit(n, o1, o2, o3):
+    while o3[-1]*2 < n:
+        o1.append(o3[-1])
+        o2.append(o3[-1])
+        o3.append(o3[-1]*2)
+    i = -1
+    while True:
+        diff = n - o3[i]
+        if diff < 0:
+            i-=1
         else:
-            while not addUp([1]+out3,dif):
+            break
+    temp_tup = addUp(o3, n)
+    if not temp_tup:
+        o1, o2, o3 = fixit(diff, o1, o2, o3)
+        temp_tup = addUp(o3, diff)
+    o1.append(temp_tup[0])
+    o2.append(temp_tup[1])
+    o3.append(diff)
+    o1 = sorted(o1)
+    o2 = sorted(o2)
+    o3 = sorted(o3)
+    return [o1, o2, o3]
+
+def get_input():
+    with open('input.txt','r') as f:
+        i=0
+        for line in f:
+            if(i!=0):
+                inputs=[int(elem) for elem in list(line.split(','))]
+            i+=1
+    return inputs
 
 
-    elif 2*out3[i]<n:
-        out3.append(2*out3[i])
-        out1.append(out3[i])
-        out2.append(out3[i])
-        i+=1
-        inputIdx+=1
+def run():
+    # inputs = get_input()
+    out1 = [1]
+    out2 = [1]
+    out3 = [1, 2]
+    # inputs = [random.randint(2, 999999999) for i in range(0,500)]
+    inputs = [2,4,19,200,6000,8000000]
+    inputs = sorted(list(dict.fromkeys(inputs)))
+    print(inputs)
+    for n in inputs: # n
+        if not binary_search_boolean(out3, n): # lg n
+            tup = addUp(out3, n) # nlogn
+            if not tup:
+                out1, out2, out3 = fixit(n, out1, out2, out3) # n^2 lg n
+                tup = addUp(out3, n) # n lg n
+            out1.append(tup[0])
+            out2.append(tup[1])
+            out3.append(n)
 
+    out3 = out3[2:]
+    for line in zip(out1, out2, out3):
+        print(line)
+
+
+run()
